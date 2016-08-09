7971c62b7961072cf072436de10396b2f7fe1f23
M. Edgar Joel <mkakajoel@gmail.com>
2016-08-09 13:16:20 +0000

    first iteration

--- palindrome.rb
+++ palindrome.rb
@@ -1,63 +1,28 @@
 require 'pry'
 
 class Palindrome
-  attr_reader :limits
-  attr_accessor :value, :factors
+  attr_reader :value, :limits
 
-  def initialize(limits)
+  def initialize(value, limits)
+    @value = value
     @limits = limits
   end
 
-  def generate
-    largest_number = limits[:max_factor]**2
-    smallest_number = limits[:min_factor]**2
-
-    largest_number.downto(smallest_number) do |number|
-      next unless palindrome?(number)
-      unless get_factors(number).empty?
-        self.value = number
-        break
-      end
-    end
-  end
-
-  def get_factors(palindrome)
-    lower_limit = (palindrome.to_f / limits[:max_factor]).ceil
+  def factors
+    lower_limit = (value.to_f / limits[:max_factor]).ceil
     lower_limit = limits[:min_factor] if lower_limit < limits[:min_factor]
-    upper_limit = palindrome / limits[:min_factor]
+    upper_limit = value / limits[:min_factor]
     upper_limit = limits[:max_factor] if upper_limit > limits[:max_factor]
     factors = []
 
     while upper_limit > lower_limit
-    	if palindrome % lower_limit == 0
-    		factors << [lower_limit, palindrome / lower_limit]
-    		upper_limit = palindrome / lower_limit
-    	end
-    	lower_limit += 1
-    end
-
-    self.factors = factors
-  end
-
-  def palindrome?(number)
-    number.to_s.chars.reverse == number.to_s.chars
-  end
-end
-
-class LargestPalindrome < Palindrome
-end
-
-class SmallestPalindrome < Palindrome
-  def generate
-    largest_number = limits[:max_factor]**2
-    smallest_number = limits[:min_factor]**2
-
-    (smallest_number..largest_number). each do |number|
-      next unless palindrome?(number)
-      unless get_factors(number).empty?
-        self.value = number
-        break
+      if value % lower_limit == 0
+        factors << [lower_limit, value / lower_limit]
+        upper_limit = value / lower_limit
       end
+      lower_limit += 1
     end
+
+    factors
   end
 end
--- palindrome_products.rb
+++ palindrome_products.rb
@@ -2,17 +2,41 @@ require 'pry'
 require_relative 'palindrome'
 
 class Palindromes
-  attr_reader :palindrome, :largest, :smallest
+  attr_accessor :palindromes
+  attr_reader :limits
 
   def initialize(limits)
     limits[:min_factor] ||= 1
-    @largest = LargestPalindrome.new(limits)
-    @smallest = SmallestPalindrome.new(limits)
+    @limits = limits
+    @palindromes = []
   end
 
   def generate
-    largest.generate
-    smallest.generate
+    self.palindromes = palindrome_values.map do |value|
+                        Palindrome.new(value, limits)
+                      end
+  end
+
+  def largest
+    palindromes.last
+  end
+
+  def smallest
+    palindromes.first
+  end
+
+  private
+
+  def palindrome_values
+    [*limits[:min_factor]..limits[:max_factor]]
+    .repeated_combination(2).to_a.collect do |pair|
+  		 prod = pair.inject(:*)
+  		 prod if palindrome?(prod)
+  	end.compact.sort
+	end
+
+  def palindrome?(number)
+    number.to_s.reverse == number.to_s
   end
 end
 
