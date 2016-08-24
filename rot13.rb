ef9c188ace18e4aed77fdf628beef651b919f246
M. Edgar Joel <mkakajoel@gmail.com>
2016-08-24 19:16:43 +0000

    refactored

--- rot13.rb
+++ rot13.rb
@@ -1,34 +1,43 @@
-encrypted_names = "Nqn Ybirynpr
-Tenpr Ubccre
-Nqryr Tbyqfgvar
-Nyna Ghevat
-Puneyrf Onoontr
-Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv
-Wbua Ngnanfbss
-Ybvf Unyog
-Pynhqr Funaaba
-Fgrir Wbof
-Ovyy Tngrf
-Gvz Orearef-Yrr
-Fgrir Jbmavnx
-Xbaenq Mhfr
-Wbua Ngnanfbss
-Fve Nagbal Ubner
-Zneiva Zvafxl
-Lhxvuveb Zngfhzbgb
-Unllvz Fybavzfxv
-Tregehqr Oynapu"
+PIONEERS = %(
+  Nqn Ybirynpr
+  Tenpr Ubccre
+  Nqryr Tbyqfgvar
+  Nyna Ghevat
+  Puneyrf Onoontr
+  Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv
+  Wbua Ngnanfbss
+  Ybvf Unyog
+  Pynhqr Funaaba
+  Fgrir Wbof
+  Ovyy Tngrf
+  Gvz Orearef-Yrr
+  Fgrir Jbmavnx
+  Xbaenq Mhfr
+  Wbua Ngnanfbss
+  Fve Nagbal Ubner
+  Zneiva Zvafxl
+  Lhxvuveb Zngfhzbgb
+  Unllvz Fybavzfxv
+  Tregehqr Oynapu
+).freeze
 
-def names(string)
+OFFSET = 13
+A_CODE = 'a'.ord
+ALPHA_COUNT = 26
+
+def decrypt_lines(string)
   string.each_line.with_object('') { |name, names| names << decrypt(name) }
 end
 
 def decrypt(string)
-  string.each_char.map do |char|
-    char = ((char.ord + 13 - 'a'.ord) % 26 + 'a'.ord).chr if ("a".."z").cover?(char)
-    char = ((char.ord + 13 - 'A'.ord) % 26 + 'A'.ord).chr if ("A".."Z").cover?(char)
-    char
-  end.join
+  string.each_char.map { |char| alpha_offset(char) }.join
 end
 
-puts names(encrypted_names)
+def alpha_offset(char)
+  return char unless ('a'..'z').cover?(char.downcase)
+
+  new_char = ((char.downcase.ord + OFFSET - A_CODE) % ALPHA_COUNT + A_CODE).chr
+  char < 'a' ? new_char.upcase : new_char
+end
+
+puts decrypt_lines(PIONEERS)
