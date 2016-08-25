1b3f47b83fb3cb8f4c50ee1e8f981ec798fb08e1
M. Edgar Joel <mkakajoel@gmail.com>
2016-08-25 00:37:35 +0000

    refactored

--- conversion/to_decimal.rb
+++ conversion/to_decimal.rb
@@ -1,23 +1,22 @@
 class ToDecimal
   DIGIT_DICTIONARY = ([*'0'..'9'] + [*'a'..'f']).zip([*0..15]).to_h
 
-  attr_reader :digits, :base, :max_digit
+  attr_reader :digits, :base
 
   def initialize(base, number)
     @digits = number.chars.map { |chr| DIGIT_DICTIONARY[chr] }
     @base = base
-    @max_digit = base - 1
   end
 
   def to_decimal
-    return 0 if invalid_input?
+    return 0 unless valid_input?
 
     digits.reverse.map.with_index do |digit, exponent|
       digit * base**exponent
     end.inject(:+)
   end
 
-  def invalid_input?
-    digits.any? { |digit| !digit || digit < 0 || digit > max_digit }
+  def valid_input?
+    digits.all? { |digit| (0...base).cover?(digit) }
   end
 end
