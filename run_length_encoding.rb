7b367088e909829ea85f27f7b79c9f122ba040d5
M. Edgar Joel <mkakajoel@gmail.com>
2016-08-29 18:24:57 +0000

    Initial commit

---
+++ run_length_encoding.rb
@@ -0,0 +1,23 @@
+# Run length encoding class
+class RunLengthEncoding
+  def self.encode(input)
+    input.scan(/((.)\2*)/).map do |arr|
+      arr.first.size > 1 ? "#{arr.first.size}#{arr.last}" : arr.first
+    end.join
+  end
+
+  def self.decode(input)
+    input.scan(/\d+.|./).map { |e| e.size > 1 ? e.slice!(-1) * e.to_i : e }.join
+  end
+end
+
+# Best solution, from Launch School
+module RunLengthEncoding2
+  def self.encode(input)
+    input.gsub(/(.)\1{1,}/) { |match| match.size.to_s + match[0] }
+  end
+
+  def self.decode(input)
+    input.gsub(/\d+\D/) { |match| match[-1] * match.to_i }
+  end
+end
