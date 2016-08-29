d96ec542cb33eaee42d5baa330c789b2e5047200
M. Edgar Joel <mkakajoel@gmail.com>
2016-08-29 16:28:09 +0000

    refactored

--- robot_name.rb
+++ robot_name.rb
@@ -1,16 +1,17 @@
 # A simple Robot names class
 class Robot
   @names = []
+  class << self; attr_accessor :names; end
   attr_accessor :name
 
   def initialize
     @name = assign_name
-    self.class.save_in_assigned_robots_names(@name)
+    self.class.names << @name
   end
 
   def assign_name
     loop do
-      name = [*'A000'..'ZZ999'].sample
+      name = [*'AA000'..'ZZ999'].sample
       return name unless self.class.names.include?(name)
     end
   end
@@ -18,15 +19,7 @@ class Robot
   def reset
     old_name = name
     self.name = assign_name
-    self.class.save_in_assigned_robots_names(name)
+    self.class.names << name
     self.class.names.delete(old_name)
-  end
-
-  def self.names
-    @names
-  end
-
-  def self.save_in_assigned_robots_names(name)
-    @names << name
   end
 end
